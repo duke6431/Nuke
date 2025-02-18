27aafe535c8de8c514532a4ec92e4226e510a271
DukeNguyen <duke@castalk.com>
2025-02-18 13:22:32 +0700

    Change: - Add Dynamic library

--- Package.swift
+++ Package.swift
@@ -14,7 +14,12 @@ let package = Package(
         .library(name: "Nuke", targets: ["Nuke"]),
         .library(name: "NukeUI", targets: ["NukeUI"]),
         .library(name: "NukeVideo", targets: ["NukeVideo"]),
-        .library(name: "NukeExtensions", targets: ["NukeExtensions"])
+        .library(name: "NukeExtensions", targets: ["NukeExtensions"]),
+
+        .library(name: "Nuke-Dynamic", type: .dynamic, targets: ["Nuke"]),
+        .library(name: "NukeUI-Dynamic", type: .dynamic, targets: ["NukeUI"]),
+        .library(name: "NukeVideo-Dynamic", type: .dynamic, targets: ["NukeVideo"]),
+        .library(name: "NukeExtensions-Dynamic", type: .dynamic, targets: ["NukeExtensions"])
     ],
     targets: [
         .target(name: "Nuke"),
